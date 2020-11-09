6127649b3b04be3d80fce6db57bf13041d430494
EC2 Default User <ec2-user@ip-172-31-0-215.ec2.internal>
2020-11-09 08:51:03 +0000

    [Update]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   resource :favorites, only: [:create, :destroy]
   resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 end
